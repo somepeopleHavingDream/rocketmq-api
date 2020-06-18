cbcc66f813a1e43600cd40078a757eeff2fcb538
yangxin <1393003255@qq.com>
2020-06-18 21:03:59 +0800

    feat: 消费者使用与Broker重试机制

--- src/main/java/org/yangxin/rocketmq/rocketmqapi/quickstart/Consumer.java
+++ src/main/java/org/yangxin/rocketmq/rocketmqapi/quickstart/Consumer.java
@@ -1,10 +1,56 @@
 package org.yangxin.rocketmq.rocketmqapi.quickstart;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.yangxin.rocketmq.rocketmqapi.constants.Const;
+
+import java.util.List;
+
 /**
  * 消费者
  *
  * @author yangxin
  * 2020/06/16 20:29
  */
+@Slf4j
 public class Consumer {
+
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("test_quick_consumer_name");
+        consumer.setNamesrvAddr(Const.NAME_SRV_ADDR);
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        consumer.subscribe("test_quick_topic", "*");
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            MessageExt messageExt = msgs.get(0);
+            try {
+                String topic = messageExt.getTopic();
+                String tags = messageExt.getTags();
+                String keys = messageExt.getKeys();
+                String msgBody = new String(messageExt.getBody(), RemotingHelper.DEFAULT_CHARSET);
+                log.info("topic: [{}], tags: [{}], keys: [{}], msgBody: [{}]", topic, tags, keys, msgBody);
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                int reconsumeTimes = messageExt.getReconsumeTimes();
+                if (reconsumeTimes >= 3) {
+                    // 记录日志
+                    // 做补偿处理
+                    log.error("重新消费次数达至3次，不再处理！");
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                }
+                return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+            }
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+
+        consumer.start();
+        log.info("consumer start...");
+    }
 }
